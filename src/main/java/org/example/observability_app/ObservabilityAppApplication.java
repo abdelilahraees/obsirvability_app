5279f70f0828755f790fea8ebedb43348ce95e35
abdelilahraees <abdelilahraees@gmail.com>
2026-09-18 12:25:31 +0100

    test

--- src/main/java/org/example/observability_app/ObservabilityAppApplication.java
+++ src/main/java/org/example/observability_app/ObservabilityAppApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObservabilityAppApplication {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(ObservabilityAppApplication.class, args);
     }
